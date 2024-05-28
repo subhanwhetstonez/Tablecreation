a29dd3e080370c16444c9b8caa96a08aebf3ee05
Subhan <subhan.suleman@whetstonez.com>
2024-05-28 15:12:48 +0500

    Added HTML and installed ejs package

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const studentRoute = require("./route.js");
 const studentFunctions = require("./controller.js");
 
 app.use(express.json());
+app.set("view engine", "ejs");
 app.use("/student", studentRoute);
 
-// app.use("/", (req, res) => {
-//   document.write(stdDisplay.rows);
-// });
+app.use("/", (req, res) => {
+  res.render(`pages/index`);
+});
 
 app.listen(3002, () => [console.log(" The server started on PORT = 3002")]);
